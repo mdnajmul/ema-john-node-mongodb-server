850b6089cd647fc5f358ea62014d0b62bbe3b611
mdnajmul <mdnajmulovi@gmail.com>
2021-11-02 16:29:14 +0600

    mongodb database connection setup

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
 const app = express();
 
 //PORT
@@ -9,10 +11,28 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3u7yr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("Database Connect.");
+  } finally {
+    //   await client.close();
+  }
+}
+
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
   res.send("Ema-John node server running.");
 });
 
 app.listen(port, () => {
-  console.log("Server running to the port ", port);
+  console.log("Server running at port ", port);
 });
